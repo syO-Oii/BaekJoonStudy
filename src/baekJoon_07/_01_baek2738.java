7680ad711b2c6dc28b3ad182a3e0df86542e1fa4
syO_Oii <as15421@naver.com>
2024-01-12 17:50:04 +0900

    0112 백준 문제 업로드 #1

---
+++ src/baekJoon_07/_01_baek2738.java
@@ -0,0 +1,36 @@
+package baekJoon_07;
+
+import java.util.Scanner;
+
+public class _01_baek2738 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[][] result = new int[N][M];
+		
+		// 결과 값 초기화
+		for(int i = 0 ; i < N ; i++) {
+			for(int j = 0; j < M ; j++) {
+				result[i][j] = 0;
+			}
+		}
+		
+		// 배열 2개 값 각각 계산
+		for(int a = 0 ; a < 2 ; a++){
+			for(int i = 0 ; i < N ; i++) {
+				for(int j = 0; j < M ; j++) {
+					result[i][j] += sc.nextInt();
+				}
+			}
+		}
+	
+		// 결과물 출력
+		for(int i = 0 ; i < N ; i++) {
+			for(int j = 0; j < M ; j++) {
+				System.out.print(result[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
