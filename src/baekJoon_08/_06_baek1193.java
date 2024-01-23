a4524c6430e9989e5182c0c3ada14366c0e68115
syO_Oii <as15421@naver.com>
2024-01-23 20:26:29 +0900

    백준 문제 풀이

--- src/baekJoon_08/_06_baek1193.java
+++ src/baekJoon_08/_06_baek1193.java
@@ -3,41 +3,42 @@ package baekJoon_08;
 import java.util.Scanner;
 
 /*
- *  1 1   1/1 
+ *  1 	  1 1/1 
  *  2~3   2 1/2 2/1
  *  4~6   3 3/1 2/2 1/3
  *  7~10  4 1/4 2/3 3/2 4/1
  *  11~15 5 5/1 4/2 3/3 2/4 1/5
- * 
+ *  ...
+ *  
  * */
+
 public class _06_baek1193 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int x = 0;
-		int y = 0;
 		int N = sc.nextInt();
-		boolean success = false;
-		int count = 0;
-		int result = 0;
-		int beforeResult = 0;
-		
-		while(!success) {
+		sc.close();
+		System.out.println(findLocation(N));
+	}
+	
+	private static String findLocation(int N) {
+		int count = 1;	// 대각선 라인
+		while(N > count) {
+			N -= count;
 			count++;
-			beforeResult = result;
-			result += count;
-			if(N <= result) {
-				N = N - beforeResult;
-				if(count % 2 == 0) {
-					x = count - (N - beforeResult) + 1;
-					y = N - beforeResult;
-				} else {
-					x = N - beforeResult;
-					y = count - (N - beforeResult) + 1;
-				}
-				success = true;
-			}			
 		}
-		System.out.println(x + "/" + y);
 		
+		int numerator;			// 분자
+		int denominator; 		// 분모
+		
+		
+		if(count % 2 == 0) {	// 대각선 라인 = 짝수 
+			numerator = N;		
+			denominator = (count - N) + 1;
+		} else {				// 대각선 라인 = 홀수
+			numerator = (count - N) + 1;
+			denominator = N;
+		}
+		
+		return numerator + "/" + denominator;
 	}
 }
